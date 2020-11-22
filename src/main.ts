3000251b921e9a9cac91de7f0bfb67eabdddbf9b
Oliver Micke <olivermicke@outlook.com>
2020-11-22 13:52:00 +0100

    perf: migrate to fastify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(3000);
 }
 bootstrap();
